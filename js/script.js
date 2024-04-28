10ff8b438e82c9af525f5565f1bff047c4a87fea
eliseevalex20 <64779288+eliseevalex20@users.noreply.github.com>
2024-04-29 01:50:06 +0300

    Edit header, add first slider

--- js/script.js
+++ js/script.js
@@ -13,3 +13,12 @@
 //             });
 //         }
 
+$(document).ready(function(){
+    $('.slider').slick({
+      autoplay: true,
+      autoplaySpeed: 5000,
+      arrows: false,
+      dots: true
+    });
+  });
+  
